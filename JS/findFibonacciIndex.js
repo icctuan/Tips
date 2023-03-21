ac378550434eeebef56cf8809d5b2860e1e12fb1
icctuan <601838356@qq.com>
2023-03-21 10:53:28 +0800

    feat: 斐波那契数列

--- JS/findFibonacciIndex.js
+++ JS/findFibonacciIndex.js
@@ -1,29 +1,17 @@
 // 给一个数，判断该数是否是斐波那契数列里的一项，若是则返回第几项，若不是则返回-1
+
 function findFibonacciIndex(num) {
-  const fibonacci = [1, 1];
-  let a = 0,
+  const fibonacci = [0, 1]; // 斐波那契数列开始数
+  let a = 0, // 指针位置
     b = 1;
 
-    while(num)
-  function Loop(num) {
-    const c = fibonacci[a] + fibonacci[b];
-    fibonacci.push(c);
+  while (num > fibonacci[b]) {
+    fibonacci.push(fibonacci[a] + fibonacci[b]);
     a++;
     b++;
-    if (c === num) {
-      console.log("final", b);
-      return b;
-    } else if (num < c) {
-      return -1;
-    } else {
-      Loop(num);
-    }
   }
-  return Loop;
-}
-const din = findFibonacciIndex();
-const a = din(5);
-console.log(a);
-// const a = findFibonacciIndex(5)
 
-// console.log(a);
+  return num === fibonacci[b] ? b : -1;
+}
+const din = findFibonacciIndex(34);
+console.log("index", din);
