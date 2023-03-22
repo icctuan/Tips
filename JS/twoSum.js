fbca5e4317e45e8f7c14ff054eb71a8299f2f6af
icctuan <601838356@qq.com>
2023-03-22 18:10:28 +0800

    feat: 两数相加

---
+++ JS/twoSum.js
@@ -0,0 +1,23 @@
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    const searchNums = nums.slice(i + 1);
+    const another = searchNums.findIndex((o) => o + nums[i] === target);
+    if (another !== -1) {
+      return [i, another + i + 1];
+    }
+  }
+};
+
+
+var _twoSum = function (nums, target) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[target - nums[i]] !== undefined) {
+      return [i, hash[target - nums[i]]];
+    }
+    hash[nums[i]] = i;
+  }
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 17));
