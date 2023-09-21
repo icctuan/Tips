42711615f63cc9b7e579a572c2010187605a4f24
icctuan <601838356@qq.com>
2023-09-21 11:29:05 +0800

    feat: 链表相加

--- Algorithm/addTwoNumbers.js
+++ Algorithm/addTwoNumbers.js
@@ -1,52 +1,67 @@
-// 数组相加，从第一位开始，大于10，向第二位进1位
+/** 定义链表 */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * 链表相加
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var addTwoNumbers = function (l1, l2) {
-  const new1 = new Map();
-  const [min, max] = l1.length > l2.length ? [l2, l1] : [l1, l2];
-  for (let i = 0; i < max.length; i++) {
-    const all = min[i] ? max[i] + min[i] : max[i];
-    const total = new1.has(i) ? new1.get(i) + all : all;
-    if (total >= 10) {
-      new1.set(i, total % 10);
-      new1.set(i + 1, 1);
+  let carry = 0,
+    head = null,
+    tail = null;
+  while (l1 || l2) {
+    const n1 = l1?.val || 0;
+    const n2 = l2?.val || 0;
+    const sum = n1 + n2 + carry;
+    if (!head) {
+      head = tail = new ListNode(sum % 10);
     } else {
-      new1.set(i, total);
+      tail.next = new ListNode(sum % 10);
+      tail = tail.next;
     }
+
+    carry = Math.floor(sum / 10);
+
+    l1 && (l1 = l1.next);
+    l2 && (l2 = l2.next);
   }
-  console.log(Array.from(new1.values()));
+
+  if (carry > 0) {
+    tail.next = new ListNode(carry);
+  }
+  return head;
 };
 
 addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]);
 addTwoNumbers([2, 4, 3], [5, 6, 4]);
 addTwoNumbers([0], [0]);
 
-// 链表相加
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
-
-var _addTwoNumbers = function (l1, l2) {
-  const initNum = l1.val + l2.val;
-  const curNum = initNum % 10;
-  let result = new ListNode(curNum);
-  let cur = result; // 这一步很关键，不能直接用result，无法构成链表
-  let curl1 = l1.next;
-  let curl2 = l2.next;
-  let nextNum = Math.floor(initNum / 10);
-  while (curl1 || curl2 || nextNum) {
-    const n1 = curl1 ? curl1.val : 0;
-    const n2 = curl2 ? curl2.val : 0;
-    const sum = n1 + n2 + nextNum;
-    nextNum = Math.floor(sum / 10);
-    cur.next = new ListNode(sum % 10);
-
-    cur = cur.next;
-    curl1 = curl1 ? curl1.next : curl1;
-    curl2 = curl2 ? curl2.next : curl2;
+/**
+ * 数组相加，用Map记录，从第一位开始，大于10，向第二位进1位
+ * */
+var addTwoArrayNumbers = function (l1, l2) {
+  const nodeMap = new Map();
+  let index = 0;
+  while (l1.length || l2.length) {
+    let n1 = l1.shift() || 0,
+      n2 = l2.shift() || 0;
+    const total = nodeMap.has(index) ? nodeMap.get(index) + n1 + n2 : n1 + n2;
+    if (total >= 10) {
+      nodeMap.set(index, total % 10);
+      nodeMap.set(index + 1, Math.floor(total / 10)); // 下一位进位
+    } else {
+      nodeMap.set(index, total);
+    }
+    index += 1;
   }
-  return result;
+  console.log(Array.from(nodeMap.values()));
 };
 
-_addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]);
-_addTwoNumbers([2, 4, 3], [5, 6, 4]);
-_addTwoNumbers([0], [0]);
+addTwoArrayNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]);
+addTwoArrayNumbers([2, 4, 3], [5, 6, 4]);
+addTwoArrayNumbers([0], [0]);
